8f8943017ec65ee736b288ecca32c4053886fa4b
Ambar Gairola <ambargairola83@gmail.com>
2024-09-04 11:31:32 +0530

    files

---
+++ Input based/input3.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    int sum;
+    int diff;
+    cin>>a;
+    cin>>b;
+    cout << "Sum is: " << a+b << endl;
+    cout << "Difference is: "<< a-b << endl;
+}
---
+++ Input based/input4.cpp
@@ -0,0 +1,9 @@
+// Complete the code below
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string x;
+  cin >> x;
+  cout << "Hello " << x;
+}
