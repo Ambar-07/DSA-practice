7049ccf9df17b901ca24d9e6820b583a4a23386c
Ambar Gairola <ambargairola83@gmail.com>
2024-09-07 22:55:46 +0530

    new

---
+++ functions/functions.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+        int A, B;
+        cin >> A >> B;
+        
+        int C = A * A + 2 * A * B + B * B;
+        int D = A + B;
+        
+        cout << C << endl;
+        cout << D << endl;
+
+        cin >> A >> B;
+        
+        C = A * A + 2 * A * B + B * B;
+        D = A + B;
+        
+        cout << C << endl;
+        cout << D << endl;
+        cin >> A >> B;
+        
+        C = A * A + 2 * A * B + B * B;
+        D = A + B;
+        
+        cout << C << endl;
+        cout << D << endl;
+    
+    
+}
