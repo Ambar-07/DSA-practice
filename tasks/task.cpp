252fad1c9c24c5e07f720100f15e146a2dc6347d
Ambar Gairola <ambargairola83@gmail.com>
2024-09-11 23:52:10 +0530

    initial commit

---
+++ tasks/task.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int A, B, C, D, E;
+    cin >> A;
+    cin >> B;
+    cin >> C;
+    cin >> D;
+    cin >> E;
+    cout << A << endl;
+    cout << B << endl;
+    cout << C << endl;
+    cout << D << endl;
+    cout << E << endl;
+}
---
+++ tasks/task2.cpp
@@ -0,0 +1,20 @@
+// Update the '_' in the code below
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    //accept the count of test cases given in the the 1st line
+    cin >> t;
+    //Run a loop to accept 't' inputs
+    while (t--)
+    {
+        int N;
+        //accept an integer N in each test case
+        cin>> N;
+        //output the number mirror for each test case
+        cout<< N<<endl;
+    }
+}
