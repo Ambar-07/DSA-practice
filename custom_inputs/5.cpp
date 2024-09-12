68a6b1f756abbdb7cd98c74ebfc80b947b6556d1
Ambar Gairola <ambargairola83@gmail.com>
2024-09-12 22:36:38 +0530

    new code

---
+++ custom_inputs/5.cpp
@@ -0,0 +1,16 @@
+// Debug the following code to solve the problem
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+    cin >> N;
+    cout << 2 * N << endl;
+  }
+}
