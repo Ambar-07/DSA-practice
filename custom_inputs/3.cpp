89e3f0f3e253ac77b3575877d3dbe4a048f41610
Ambar Gairola <ambargairola83@gmail.com>
2024-09-12 22:31:22 +0530

    new commit

---
+++ custom_inputs/3.cpp
@@ -0,0 +1,20 @@
+// Update the '_' in the code below
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    //accept the count of test cases given in the the 1st line
+    cin >> t;
+    //Run a loop to accept 't' inputs
+    while (t--)
+    {
+        int N;
+        //accept 1 integer on the 1st line of each test case
+        cin >> N;
+        //output the negative integer - i.e. (-N)
+        cout << -N << endl;
+    }
+}
