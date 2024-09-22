55c00131883930427db9d606e52087291f0e6b03
Ambar Gairola <ambargairola83@gmail.com>
2024-09-22 21:55:31 +0530

    new

---
+++ Practice/12.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+         cout<< 4*3<<endl;
+         cout<< 4*1<<endl;
+         cout<< 2*1<<endl;
+         cout<< 4*8;
+         
+    
+}
