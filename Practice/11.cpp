4c1b5365cff1cfbae50940676b67eb22a4fab392
Ambar Gairola <ambargairola83@gmail.com>
2024-09-22 21:49:39 +0530

    new

---
+++ Practice/11.cpp
@@ -0,0 +1,10 @@
+ #include <iostream>
+using namespace std;
+
+int main() {
+	     cout<< "1" << "-" << 1*1 << endl;
+	      cout<< "2" << "-" << 2*2 << endl;
+	       cout<< "3" << "-" << 3*3 << endl;
+	        cout<< "4" << "-" << 4*4 << endl;
+	         cout<< "5" << "-" << 5*5 << endl;
+}
