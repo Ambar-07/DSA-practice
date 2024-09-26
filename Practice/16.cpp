f439e602d86bba98ea79518aabb44231a3bdc37a
Ambar Gairola <ambargairola83@gmail.com>
2024-09-26 23:36:38 +0530

    new

---
+++ Practice/16.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int hour = 5;
+    int minutes = hour * 60;
+    int seconds = minutes * 60;
+    cout << minutes << endl <<seconds;
+}
