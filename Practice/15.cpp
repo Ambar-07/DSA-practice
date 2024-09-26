77ddb279745485dc5fccc3dce45297be1ae9f042
Ambar Gairola <ambargairola83@gmail.com>
2024-09-26 23:32:43 +0530

    new

---
+++ Practice/15.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double temperature = 25.5;
+    cout << "Celsius - " << temperature << endl;
+    cout << "Kelvin - " << temperature + 273;
+
+    return 0;
+}
